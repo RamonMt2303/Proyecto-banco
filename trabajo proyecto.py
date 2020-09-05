2b77f65fe38cd9ced9369c4335a2969bc11aab5b
RamonMt2303 <69860663+RamonMt2303@users.noreply.github.com>
2020-09-05 18:02:43 -0500

    Add files via upload

--- trabajo proyecto.py
+++ trabajo proyecto.py
@@ -3,31 +3,25 @@
 print ("Bienvenido usuario")
 #Este va a ser la interfaz de el programa
 print ("¿Cuánto dinero desea ingresar?")
-ahorro = 0
-#Esto lo metí porque quiero meter la opción de si/no pero no me salió al momento de realizar esta entrega, asi que no los usaré esta priemra entrega
-si = 1
-no = 0
-deposito = int(input("Ingrese el monto a depositar en números enteros y sin decimales"))
+saldo = 0
+
+deposito = float(input("Ingrese la cantidad a depositar"))
 while deposito <= 0:
     print("Eso no es un número válido")
-    
-    deposito = int(input("Ingrese un número entero y sin decimales válido"))
-    if deposito == 0:
-        print("Usted escribió 0, eso no es un número válido")
-        deposito = int(input("Ingrese un número entero y sin decimales válido"))
-           
+    deposito = float(input("Ingrese un número entero y sin decimales válido"))
+  
 print("Accediendo al sistema")
 
-print("Usted está a punto de depositar:",deposito,"¿es correcto?")
+print("Usted está a punto de depositar:",deposito,"¿Es correcto?")
 confirmacion = int(input("Ingrese 0 si desea cancelarlo, o 1 si desea continuar"))
-#def confirmacion (1, 0):
 if confirmacion == 0:
     print ("Cancelando transacción")
 else:
     print ("Accediendo al sistema")
-    sum = ahorro + deposito
+    sum = saldo + deposito
     saldo = sum
     print ("Su saldo actual es de:",saldo)
+    
 print("Gracias por usar nuestro servicio bancario, vuelva pronto")
 
 
