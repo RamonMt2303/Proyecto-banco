e608aee1b8a28b0c1f4d10b5e31d78720fb26109
RamonMt2303 <69860663+RamonMt2303@users.noreply.github.com>
2020-09-11 23:04:14 -0500

    Add files via upload
    Voy a subir el mismo programa de la semana pasada, solo que lo modifiqué para que sea más fácil de leer para el programador

--- trabajo proyecto.py
+++ trabajo proyecto.py
@@ -1,40 +1,49 @@
 #Proyecto github
 
-print ("Bienvenido usuario")
+#Funciones
+def saldo_cuenta (ahorro, retiro):
+    saldo = ahorro - retiro
+    return saldo
+
+#Variables globales
+saldo = 0
+retiro = 0
+
+print ("Bienvenido usuario\n")
 #Este va a ser la interfaz de el programa
 print ("¿Cuánto dinero desea ingresar?")
-saldo = 0
 
-deposito = float(input("Ingrese la cantidad a depositar"))
+deposito = float(input("Ingrese la cantidad a depositar:\n"))
+
 while deposito <= 0:
     print("Eso no es un número válido")
-    deposito = float(input("Ingrese una cantidad válida"))
+    deposito = float(input("Ingrese una cantidad válida:\n"))
   
-print("Accediendo al sistema")
+print(".........Accediendo al sistema.........")
 
-print("Usted está a punto de depositar:",deposito,"¿Es correcto?")
-confirmacion = int(input("Ingrese 0 si desea cancelarlo, o 1 si desea continuar"))
+print("Usted está a punto de depositar:\t",deposito,"\n¿Es correcto?")
+confirmacion = int(input("Ingrese 0 si desea cancelarlo o 1 si desea continuar\n"))
 
 if confirmacion == 0:
     print ("Cancelando transacción")
 else:
-    print ("Accediendo al sistema")
-    sum = saldo + deposito
-    saldo = sum
-    print ("Su saldo actual es de:",saldo)
+    saldo = saldo + deposito
+    print ("Su saldo actual es de:\t",saldo, "dólares")
 
 ahorro = saldo
+
 print ("¿Desea hacer algún retiro?")
-confirmacion_retiro = int(input("Ingrese 2 si desea cancelarlo, o 3 si desea continuar"))
-if confirmacion_retiro == 2:
+confirmacion_retiro = int(input("Ingrese 0 si desea cancelarlo, o 1 si desea continuar\n"))
+if confirmacion_retiro == 0:
     print ("Cancelando transacción")
-else:
+elif confirmacion_retiro == 1:
     print ("Accediendo al sistema")
     retiro = float(input("Ingrese el monto a retirar"))
-    def saldo (ahorro, retiro):
-        return ahorro - retiro
-    
-print(saldo (ahorro , retiro))
+    while retiro > ahorro:
+        print("Porfavor intente otra cantidad")
+        retiro = float(input("Ingrese el monto a retirar"))
+    print ("Tu saldo actual es de:\t",saldo_cuenta(ahorro,retiro), "dólares")
+
     
 print("Gracias por usar nuestro servicio bancario, vuelva pronto")
 
