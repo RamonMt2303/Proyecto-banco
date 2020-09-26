cc04280727ebbdbd587bccabebeb3b0a42b64786
RamonMt2303 <69860663+RamonMt2303@users.noreply.github.com>
2020-09-25 19:38:16 -0500

    Add files via upload
    Volveré a subir el mismo documento de la entrega pasada, solo agregué la recomendación de el avance 3 de si quiero cancelar el depósito desde el inicio, me saca directamente del programa

--- trabajo proyecto.py
+++ trabajo proyecto.py
@@ -9,43 +9,48 @@ def saldo_cuenta (ahorro, retiro):
 saldo = 0
 retiro = 0
 
+print ("Bienvenido a MazeBank\n")
 print ("Bienvenido usuario\n")
 #Este va a ser la interfaz de el programa
-print ("¿Cuánto dinero desea ingresar?")
+confirmacion_menu = int(input ("¿Desea hacer un depósito? ----- Ingrese 1 para acceder al sistema, o 0 para salir"))
+if confirmacion_menu == 1:
+    print(".........Accediendo al sistema.........")
 
-deposito = float(input("Ingrese la cantidad a depositar:\n"))
+    print ("¿Cuánto dinero desea depositar a su cuenta?")
 
-while deposito <= 0:
-    print("Eso no es un número válido")
-    deposito = float(input("Ingrese una cantidad válida:\n"))
-  
-print(".........Accediendo al sistema.........")
+    deposito = float(input("Ingrese la cantidad a depositar:\n"))
 
-print("Usted está a punto de depositar:\t",deposito,"\n¿Es correcto?")
-confirmacion = int(input("Ingrese 0 si desea cancelarlo o 1 si desea continuar\n"))
+    while deposito <= 0:
+        print("Eso no es un número válido")
+        deposito = float(input("Ingrese una cantidad válida:\n"))
+      
+    print(".........Accediendo al sistema.........")
 
-if confirmacion == 0:
-    print ("Cancelando transacción")
-else:
-    saldo = saldo + deposito
-    print ("Su saldo actual es de:\t",saldo, "dólares")
+    print("Usted está a punto de depositar:\t",deposito,"\n¿Es correcto?")
+    confirmacion = int(input("Ingrese 1 si desea continuar o 0 si desea cancelar\n"))
 
-ahorro = saldo
+    if confirmacion == 0:
+        print ("Cancelando transacción")
+    else:
+        saldo = saldo + deposito
+        print ("Su saldo actual es de:\t",saldo, "de dólares")
 
-print ("¿Desea hacer algún retiro?")
-confirmacion_retiro = int(input("Ingrese 0 si desea cancelarlo, o 1 si desea continuar\n"))
-if confirmacion_retiro == 0:
-    print ("Cancelando transacción")
-elif confirmacion_retiro == 1:
-    print ("Accediendo al sistema")
-    retiro = float(input("Ingrese el monto a retirar"))
-    while retiro > ahorro:
-        print("Porfavor intente otra cantidad")
+    ahorro = saldo
+
+    print ("¿Desea hacer algún retiro?")
+    confirmacion_retiro = int(input("Ingrese 1 si desea continuar o 0 si desea cancelar\n"))
+    if confirmacion_retiro == 0:
+        print ("Cancelando transacción")
+    elif confirmacion_retiro == 1:
+        print ("Accediendo al sistema")
         retiro = float(input("Ingrese el monto a retirar"))
-    print ("Tu saldo actual es de:\t",saldo_cuenta(ahorro,retiro), "dólares")
-
+        while retiro > ahorro:
+            print("Porfavor intente otra cantidad")
+            retiro = float(input("Ingrese el monto a retirar"))
+        print ("Tu saldo actual es de:\t",saldo_cuenta(ahorro,retiro), "dólares")
+else:
     
-print("Gracias por usar nuestro servicio bancario, vuelva pronto")
+    print("Gracias por usar nuestro servicio bancario, vuelva pronto")
 
 
 
